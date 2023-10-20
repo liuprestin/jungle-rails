e1db7b3462f5a8056c1aa07e9570e801b7de90ed
Prestin Liu <liuprestin@proton.me>
2023-10-19 21:35:12 -0700

    remove demo sessioncontroller - generate a session controller with new/create/destroy

--- app/controllers/SessionsController.rb
+++
@@ -1,7 +0,0 @@
-# def create
-#   if user = User.authenticate_with_credentials(params[:email], params[:password])
-#     # success logic, log them in
-#   else
-#     # failure, render login form
-#   end
-# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   get 'about', to: 'about#index'
   root to: 'products#index'
 
